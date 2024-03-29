ae741e5178017675b31e5396fa3508b65bda84af
theduy21092001 <165370000+theduy21092001@users.noreply.github.com>
2024-03-29 16:28:35 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost:8080/TM/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost:8080/TM/');
+
+// DIR
+define('DIR_APPLICATION', 'D:/zzzz/USBWebserver v8.6/root/TM/catalog/');
+define('DIR_SYSTEM', 'D:/zzzz/USBWebserver v8.6/root/TM/system/');
+define('DIR_IMAGE', 'D:/zzzz/USBWebserver v8.6/root/TM/image/');
+define('DIR_LANGUAGE', 'D:/zzzz/USBWebserver v8.6/root/TM/catalog/language/');
+define('DIR_TEMPLATE', 'D:/zzzz/USBWebserver v8.6/root/TM/catalog/view/theme/');
+define('DIR_CONFIG', 'D:/zzzz/USBWebserver v8.6/root/TM/system/config/');
+define('DIR_CACHE', 'D:/zzzz/USBWebserver v8.6/root/TM/system/storage/cache/');
+define('DIR_DOWNLOAD', 'D:/zzzz/USBWebserver v8.6/root/TM/system/storage/download/');
+define('DIR_LOGS', 'D:/zzzz/USBWebserver v8.6/root/TM/system/storage/logs/');
+define('DIR_MODIFICATION', 'D:/zzzz/USBWebserver v8.6/root/TM/system/storage/modification/');
+define('DIR_UPLOAD', 'D:/zzzz/USBWebserver v8.6/root/TM/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'usbw');
+define('DB_DATABASE', 'test');
+define('DB_PORT', '3307');
+define('DB_PREFIX', 'oc_');
